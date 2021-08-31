ab112f34e61b21f0d2435e0bae3d5ef5b803f8f3
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-31 09:02:18 +0200

    introduced the notebook approach

--- batch.py
+++ batch.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sample_generator import SampleGenerator
+from typing import Tuple
 
 
 class Batch:
@@ -7,21 +8,28 @@ class Batch:
         self.x_data = []
         self.y_data = []
         self.batch_size = 50
+        self.g = SampleGenerator()
 
     def reset_batch(self) -> None:
         self.x_data = []
         self.y_data = []
+
+    def add_non_linear_sample(self) -> Tuple[float, float]:
+        x = np.random.random()
+        y = self.g.generate_non_linear_samples(x)
+        self.x_data.append(x)
+        self.y_data.append(y)
+        return x, y
 
     def make_nonlinear_batch_data(self) -> None:
         """ 
         Generate a batch of non linear data and store it into numpy structures
         """
         self.reset_batch()
-        g = SampleGenerator()
         for i in range(self.batch_size):
             # Draw a random sample on the interval [0,1]
             x = np.random.random()
-            y = g.generate_non_linear_samples(x)
+            y = self.g.generate_non_linear_samples(x)
             self.x_data.append(x)
             self.y_data.append(y)
 
@@ -30,10 +38,9 @@ class Batch:
         Generate a batch of linear data and store it into numpy structures
         """
         self.reset_batch()
-        g = SampleGenerator()
         for i in range(self.batch_size):
             # Draw a random sample on the interval [0,1]
             x = np.random.random()
-            y = g.generate_linear_samples(x)
+            y = self.g.generate_linear_samples(x)
             self.x_data.append(x)
             self.y_data.append(y)
--- main.py
+++ main.py
@@ -5,113 +5,66 @@ import time
 from rbfn import RBFN
 from lwr import LWR
 from line import Line
-from sample_generator import SampleGenerator
+from batch import Batch
 
+# Linear Least Squares
 
-class Main:
-    def __init__(self):
-        self.x_data = []
-        self.y_data = []
-        self.batch_size = 50
+batch = Batch()
+model = Line(batch.batch_size)
+batch.make_linear_batch_data()
+start = time.process_time()
+model.train(batch.x_data, batch.y_data)
+print("LLS time:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-    def reset_batch(self):
-        self.x_data = []
-        self.y_data = []
+start = time.process_time()
+model.train_from_stats(batch.x_data, batch.y_data)
+print("LLS from scipy stats:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-    def make_nonlinear_batch_data(self):
-        """ 
-        Generate a batch of non linear data and store it into numpy structures
-        """
-        self.reset_batch()
-        g = SampleGenerator()
-        for i in range(self.batch_size):
-            # Draw a random sample on the interval [0,1]
-            x = np.random.random()
-            y = g.generate_non_linear_samples(x)
-            self.x_data.append(x)
-            self.y_data.append(y)
+start = time.process_time()
+model.train_regularized(batch.x_data, batch.y_data, coef=0.01)
+print("regularized LLS :", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-    def make_linear_batch_data(self):
-        """ 
-        Generate a batch of linear data and store it into numpy structures
-        """
-        self.reset_batch()
-        g = SampleGenerator()
-        for i in range(self.batch_size):
-            # Draw a random sample on the interval [0,1]
-            x = np.random.random()
-            y = g.generate_linear_samples(x)
-            self.x_data.append(x)
-            self.y_data.append(y)
+# Batch RBFNs
 
-    def approx_linear_batch(self):
-        model = Line(self.batch_size)
+model = RBFN(nb_features=10)
+batch.make_nonlinear_batch_data()
 
-        self.make_linear_batch_data()
+start = time.process_time()
+model.train_ls(batch.x_data, batch.y_data)
+print("RBFN LS time:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-        start = time.process_time()
-        model.train(self.x_data, self.y_data)
-        print("LLS time:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
+start = time.process_time()
+model.train_ls2(batch.x_data, batch.y_data)
+print("RBFN LS2 time:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-        start = time.process_time()
-        model.train_from_stats(self.x_data, self.y_data)
-        print("LLS from scipy stats:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
+# Incremental RBFNs
 
-        start = time.process_time()
-        model.train_regularized(self.x_data, self.y_data, coef=0.01)
-        print("regularized LLS :", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
+max_iter = 50
+model = RBFN(nb_features=10)
+start = time.process_time()
 
-    def approx_rbfn_batch(self):
-        model = RBFN(nb_features=10)
-        self.make_nonlinear_batch_data()
+# Generate a batch of data and store it
+batch.reset_batch()
+for i in range(max_iter):
+    x, y = batch.add_non_linear_sample()
+    model.train_gd(x, y, alpha=0.5)
+    # model.train_rls(x, y)
+    # model.train_rls_sherman_morrison(x, y)
 
-        start = time.process_time()
-        model.train_ls(self.x_data, self.y_data)
-        print("RBFN LS time:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
+print("RBFN Incr time:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
 
-        start = time.process_time()
-        model.train_ls2(self.x_data, self.y_data)
-        print("RBFN LS2 time:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
+# LWR
 
-    def approx_rbfn_iterative(self):
-        max_iter = 50
-        model = RBFN(nb_features=10)
-        start = time.process_time()
-        # Generate a batch of data and store it
-        self.reset_batch()
-        g = SampleGenerator()
-        for i in range(max_iter):
-            # Draw a random sample on the interval [0,1]
-            x = np.random.random()
-            y = g.generate_non_linear_samples(x)
-            self.x_data.append(x)
-            self.y_data.append(y)
+model = LWR(nb_features=10)
+batch.make_nonlinear_batch_data()
 
-            # Comment the ones you don't want to use
-            model.train_gd(x, y, alpha=0.5)
-            # model.train_rls(x, y)
-            # model.train_rls_sherman_morrison(x, y)
-
-        print("RBFN Incr time:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
-
-    def approx_lwr_batch(self):
-        model = LWR(nb_features=10)
-        self.make_nonlinear_batch_data()
-
-        start = time.process_time()
-        model.train_lwls(self.x_data, self.y_data)
-        print("LWR time:", time.process_time() - start)
-        model.plot(self.x_data, self.y_data)
-
-if __name__ == '__main__':
-    m = Main()
-    # m.approx_linear_batch()
-    # m.approx_rbfn_batch()
-    m.approx_rbfn_iterative()
-    m.approx_lwr_batch()
+start = time.process_time()
+model.train_lwls(batch.x_data, batch.y_data)
+print("LWR time:", time.process_time() - start)
+model.plot(batch.x_data, batch.y_data)
