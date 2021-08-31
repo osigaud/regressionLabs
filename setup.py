62f1253b03500da4b9ac5ef7ac5ed544ee18c76e
osigaud <Olivier.Sigaud@upmc.fr>
2021-08-31 11:19:27 +0200

    added steup, removed old code

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+from setuptools import setup
+
+with open(os.path.join("regression_labs", "version.txt"), "r") as file_handler:
+    __version__ = file_handler.read().strip()
+
+# Taken from PyTorch code to have a different version per commit
+hash = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=".").decode("ascii").strip()
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name="regressionLabs",
+    url="https://github.com/osigaud/regressionLabs",
+    author="Olivier Sigaud",
+    author_email="Olivier.Sigaud@upmc.fr",
+    # Needed to actually package something
+    packages=["regression_labs"],
+    # Needed for dependencies
+    install_requires=["numpy", "opencv-python"],
+    # *strongly* suggested for sharing
+    version=f"{__version__}.dev0+{hash}",
+    # The license can be anything you like
+    license="MIT",
+    description="Simple regression labs to approximate a one-dimensional function",
+    long_description=open("README.md").read(),
+)
